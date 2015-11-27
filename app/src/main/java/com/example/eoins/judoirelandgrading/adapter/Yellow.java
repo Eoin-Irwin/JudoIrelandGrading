b938af1e498960c8b6f033b50d2052d1fcf452c3
Eoin Irwin <C13439802@mydit.ie>
2015-11-27 18:28:02 +0000

    Removed redundant code

--- app/src/main/java/com/example/eoins/judoirelandgrading/adapter/Yellow.java
+++ app/src/main/java/com/example/eoins/judoirelandgrading/adapter/Yellow.java
@@ -7,22 +7,12 @@ package com.example.eoins.judoirelandgrading.adapter;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ExpandableListView;
-
-import com.example.eoins.judoirelandgrading.R;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 
 public class Yellow extends Fragment {
-    LinkedHashMap<String, List<String>> Judo_category;
-    List<String> judoMoves;
-    ExpandableListView Exp_list;
-    JudoAdapter adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -99,23 +89,8 @@ public class Yellow extends Fragment {
         JudoMoves.put("Gonosen No Kata", GonosenNoKata);
         JudoMoves.put("Terminology", Terminology);
 
-
         return JudoMoves;
 
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.yellow, container, false);
-        Exp_list = (ExpandableListView) rootView.findViewById(R.id.exp_list);
-        Judo_category = Yellow.getInfo();
-        judoMoves = new ArrayList<String>(Judo_category.keySet());
-        adapter = new JudoAdapter(getActivity(), Judo_category, judoMoves);
-        Exp_list.setAdapter(adapter);
-
-        // Inflate the layout for this fragment
-        return rootView;
     }
 
     @Override
